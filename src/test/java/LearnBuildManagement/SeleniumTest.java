0f097c8a07df4a60565622ab0c23ee9fee1b0215
jitendrakush <35888508+jitendrakush@users.noreply.github.com>
2022-03-03 00:40:49 +0530

    Added SeleniumTest Class file (#2)

---
+++ src/test/java/LearnBuildManagement/SeleniumTest.java
@@ -0,0 +1,10 @@
+package LearnBuildManagement;
+
+import org.junit.Test;
+
+public class SeleniumTest {
+    @Test
+    public void printHello(){
+        System.out.println("My Name is : Jitendra");
+    }
+}
