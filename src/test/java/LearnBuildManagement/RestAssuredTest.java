0447ec03cc4946927d502b4ba7e1808c872537ea
Jitendra Kushwah <jitendrakushwah1994@gmail.com>
2022-03-03 00:19:19 +0530

    Initial commit

---
+++ src/test/java/LearnBuildManagement/RestAssuredTest.java
@@ -0,0 +1,11 @@
+package LearnBuildManagement;
+
+import org.junit.Test;
+
+public class RestAssuredTest {
+
+	@Test
+	public void nativeAndroidApp() {
+		System.out.println("nativeAndroidApp");
+	}
+}
